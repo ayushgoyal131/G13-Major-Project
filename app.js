0c4df26e98bb021a42cb4ae29354c4a312aa2749
sam0405 <samridhi32in@gmail.com>
2021-11-12 19:07:56 +0530

    adding order feature

--- app.js
+++ app.js
@@ -197,6 +197,17 @@ app.post('/seller/signup', function(req, res){
   });
 });
 
+
+app.get('/orders', function(req, res){
+  res.render('orders.ejs', {});
+})
+app.get('/orders/your-orders', function(req, res){
+  res.redirect('/orders');
+})
+app.get('/orders/cancelled-orders', function(req, res){
+  res.render('order_cancel.ejs', {});
+})
+
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/seller');
